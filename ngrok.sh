a4c0fcdb1d351954dccb0081f41339cf78488b15
AHMDEV0 <ahmthedev@gmail.com>
2024-11-02 21:26:53 +0530

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,4 +1,4 @@
 # FOR x86 servers using linux : 
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -xf ngrok-v3-stable-linux-amd64.tgz
-./ngrok config add-authtoken 2XqE5yZqoTUiJ52tQxqZ6KXW4JJ_vMgD4KSewbGD8XYrkRUj
+./ngrok config add-authtoken 2mvoVYq0i3AqYyf7AdyKvoAKBEh_4jzvcYnLQQ8Q4fF6exYTR
